5002bc88916e3960f35a01cb0454aa092bea6602
Tim Fechner <tim.fechner@mailbox.org>
2024-03-03 22:55:49 +0100

    Make calendars barely configurable

--- src/App.vue
+++ src/App.vue
@@ -54,6 +54,22 @@ function getAllCalendarEvents() {
   })
 }
 
+function removeCalendar(index) {
+  calendarEvents.value[calendarConfiguration.value[index].url] = [];
+  calendarConfiguration.value.splice(index, 1);
+  getAllCalendarEvents();
+}
+
+function addCalendar() {
+  calendarConfiguration.value.push({
+    url: "",
+    order: 0,
+    color: "#000000",
+    updateInterval: 300,
+    lastUpdate: null,
+  })
+}
+
 
 onMounted(() => {
   getAllCalendarEvents();
@@ -62,12 +78,70 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex flex-row bg-gray-100">
-    <div class="basis-9/12">
-      <EventCalendar :calendar-events="calendarEvents" />
+  <!-- calendar configuration -->
+  <div class="bg-gray-100">
+    <div class="pt-4 ">
+      <div class="bg-white rounded shadow mx-4 px-2 py-2">
+        <div
+          v-for="(conf, index) in calendarConfiguration"
+          :key="conf.url"
+          class="flex gap-4 mb-4"
+        >
+          <div>
+            <input
+              :id="'color-' + index"
+              v-model="conf.color"
+              type="color"
+              class="invisible h-0 w-0"
+            >
+            <label
+              :for="'color-' + index"
+              class="w-16 h-full inline-block rounded border shadow"
+              :style="'background-color: ' + conf.color + ';'"
+            />
+          </div>
+          <div class="grow">
+            <input
+              v-model="conf.url"
+              type="text"
+              class="border rounded shadow px-2 truncate w-full"
+            >
+          </div>
+          <!-- <div><input type="number" v-model="conf.updateInterval" class="border rounded shadow px-2 w-fit"></div> -->
+          <div>
+            <button
+              class="border rounded shadow block"
+              @click="removeCalendar(index)"
+            >
+              &#128465;
+            </button>
+          </div>
+        </div>
+        <div class="flex gap-2">
+          <button
+            class="px-4 py-2 rounded border shadow font-semibold"
+            @click="getAllCalendarEvents()"
+          >
+            Save & Reload
+          </button>
+          <button
+            class="px-4 py-2 rounded border shadow font-semibold"
+            @click="addCalendar()"
+          >
+            &#10133; Add Calendar
+          </button>
+        </div>
+      </div>
     </div>
-    <div class="basis-3/12">
-      <EventList :calendar-events="calendarEvents" />
+
+    <!-- calendar & list view -->
+    <div class="flex flex-row">
+      <div class="basis-9/12">
+        <EventCalendar :calendar-events="calendarEvents" />
+      </div>
+      <div class="basis-3/12">
+        <EventList :calendar-events="calendarEvents" />
+      </div>
     </div>
   </div>
 </template>
